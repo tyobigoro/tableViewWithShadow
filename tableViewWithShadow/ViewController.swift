da3d660b12c5fa57d7a2851719825c3925aa2ec0
tyobigoro_i <tyobigorox@gmail.com>
2020-04-27 19:04:15 +0900

    d

--- tableViewWithShadow/ViewController.swift
+++ tableViewWithShadow/ViewController.swift
@@ -35,7 +35,7 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 20
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
